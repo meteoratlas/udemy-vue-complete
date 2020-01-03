90f7bc2b61c23e3f79bb9ab86b48bc9154d1ad20
Kelly H <kellyaccnts@gmail.com>
2020-01-02 19:50:56 -0700

    gameOver state, visual improvement

--- projects/Monster Slayer/js/app.js
+++ projects/Monster Slayer/js/app.js
@@ -1,37 +1,28 @@
 new Vue({
     el: "#app",
     data: {
+        gameOver: true,
         playerHP: 100,
-        playerAttackPower: 9,
-        playerSpecialPower: 16,
-        playerHealPower: 8,
+        playerAttackPower: 11,
+        playerSpecialPower: 19,
+        playerHealPower: 10,
         monsterHP: 100,
-        monsterAttackPower: 8,
-        messages: [],
-        pHPStyle: {
-            backgroundColor: "green",
-            margin: 0,
-            color: "white",
-            width: this.playerHP + "%"
-        },
-        mHPStyle: {}
+        monsterAttackPower: 11,
+        messages: []
     },
     methods: {
         playerAttack: function(power) {
-            if (this.playerHP <= 0 || this.monsterHP <= 0) return;
+            if (this.gameOver) return;
             const attack = Math.max(Math.floor(Math.random() * power), 3);
             this.monsterHP -= attack;
-            if (this.monsterHP <= 0) {
-                this.messages.unshift(
-                    `Player hit monster for ${attack}, defeating it! Monster was vanquished!`
-                );
-            } else {
+            this.checkWinLose(attack);
+            if (!this.gameOver) {
                 this.messages.unshift(`Player hit monster for ${attack}!`);
             }
             this.monsterAttack();
         },
         playerHeal: function() {
-            if (this.monsterHP <= 0 || this.playerHP <= 0) return;
+            if (this.gameOver) return;
             this.playerHP += this.playerHealPower;
             if (this.playerHP > 100) this.playerHP = 100;
             this.messages.unshift(
@@ -40,19 +31,32 @@ new Vue({
             this.monsterAttack();
         },
         monsterAttack: function() {
-            if (this.monsterHP <= 0 || this.playerHP <= 0) return;
+            if (this.gameOver) return;
             const attack = Math.max(
                 Math.floor(Math.random() * this.monsterAttackPower),
                 3
             );
             this.playerHP -= attack;
+            this.checkWinLose(attack);
+            if (!this.gameOver)
+                this.messages.unshift(`Monster hit player for ${attack}!`);
+        },
+        checkWinLose: function(attack) {
             if (this.playerHP <= 0) {
+                this.gameOver = true;
                 this.messages.unshift(
                     `Monster hit player for ${attack} - you are defeated!`
                 );
-            } else this.messages.unshift(`Monster hit player for ${attack}!`);
+            }
+            if (this.monsterHP <= 0) {
+                this.gameOver = true;
+                this.messages.unshift(
+                    `Player hit monster for ${attack}, defeating it! Monster was vanquished!`
+                );
+            }
         },
         resetGame: function() {
+            this.gameOver = false;
             this.playerHP = 100;
             this.monsterHP = 100;
             this.messages = [];

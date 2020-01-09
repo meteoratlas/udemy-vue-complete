f80ca6cf488a570f3c4a9ce7d4439f59b254a995
Kelly H <kellyaccnts@gmail.com>
2020-01-08 19:46:18 -0700

    updated to @vue-cli

--- projects/WonderfulQuotes/src/components/Quote.vue
+++ projects/WonderfulQuotes/src/components/Quote.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-sm-6 col-md-4 col-lg-3 ">
         <div class="panel panel-default">
-            <div class="body-panel quote">
+            <div class="panel-body quote">
                 <slot> </slot>
             </div>
         </div>
